2aac44297c2206d9286df047721d5381f14b3db6
BhavanJ <bhavan.jasani@gmail.com>
2019-02-20 20:13:06 -0500

    Updated sensor module with probality functions

--- scripts/SensorModel.py
+++ scripts/SensorModel.py
@@ -3,6 +3,7 @@ import math
 import time
 from matplotlib import pyplot as plt
 from scipy.stats import norm
+import scipy.integrate as integrate
 import pdb
 
 from MapReader import MapReader
@@ -19,6 +20,43 @@ class SensorModel:
         """
         TODO : Initialize Sensor Model parameters here
         """
+        self.zmax = 100
+        self.lambda_short = 10
+        self.sigma_sq_hit = 10
+
+
+    def get_Nu(z_tk,z_tk_star):
+        return 1.0/math.sqrt(2*math.pi*self.sigma_sq_hit)*math.exp( ((z_tk - z_tk_star)**2)/(-2.0*self.sigma_sq_hit)  )
+
+    def get_p_hit(z_tk,z_tk_star):
+
+        if 0 <= z_tk <= self.zmax:
+            Nu = self.get_Nu(z_tk,z_tk_star)
+            eta = 1.0/(integrate.quad(lambda x: self.get_Nu(x,z_tk_star), 0, self.zmax))
+            return eta*Nu
+        else:
+            return 0.0
+
+    def get_p_short(z_tk,z_tk_star):
+        if 0 <= z_tk <= z_tk_star:
+            eta = 1.0/(1.0 - math.exp(-1.0*self.lambda_short*z_tk_star) )
+            return eta*(self.lambda_short)*math.exp(-1.0*self.lambda_short*z_tk)
+        else:
+            return 0.0
+
+    def get_p_max(z_tk):
+        #TODO:BJ Verify
+        if z_tk == self.zmax:
+            return 1.0
+        else:
+            return 0.0
+
+    def get_p_rand(z_tk):
+        if 0<= z_tk < self.zmax:
+            return 1.0/self.zmax
+        else:
+            return 0.0
+
 
     def beam_range_finder_model(self, z_t1_arr, x_t1):
         """
